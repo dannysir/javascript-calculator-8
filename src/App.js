20ee8454dbacc5fe43f7a4ed0ac373643200db57
dannysir <dannysir@naver.com>
2025-10-16 19:19:38 +0900

    feat : Console API를 활용한 입출력
    Console API를 이용한 입력 기능 추가.
    Console API를 이용한 결과 출력 기능 추가.
    요구 사항을 위한 run 함수 로직 설계.
    기능 추가를 위한 유틸 파일 생성 및 주요 기능 함수 선언.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
+import { findCustomDiv, splitString } from './utils.js';
 
 class App {
   async run() {
-    await MissionUtils.Console.print('1111');
+    const input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    let customString = null;
+    if (input[0] === '/') {
+      const cs = findCustomDiv(input);
+      if (!cs) {
+        // 예외처리
+      } else {
+        customString = cs;
+      }
+    }
+
+    const inputArr = splitString(input, [',', ':', customString]);
+    const OUTPUT_STRING = '결과 : ';
+    MissionUtils.Console.print(OUTPUT_STRING + inputArr.reduce((acc, cur) => acc + cur, 0));
   }
 }
 
---
+++ src/utils.js
@@ -0,0 +1,14 @@
+export const findCustomDiv = (input) => {
+  let answer = '';
+
+  for (let i = 0; i < input.length; i++) {
+    const str = input[i];
+  }
+
+  return answer;
+};
+
+export const splitString = (input, div) => {
+  let answer = [];
+  return answer;
+};
