6572226b9a8fd755162037ded53b7ff668c98be7
dannysir <dannysir@naver.com>
2025-10-19 14:05:00 +0900

    feat : 잘못된 입력이 들어올 경우 예외 처리
    음수 입력, 공백 입력, 잘못된 구분자 입력시 [ERROR]로 시작하는 에러 발생.
    불필요한 정규 표현식 제거.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
-import { findCustomDiv, splitString } from './utils.js';
+import { checkValid, ERROR_STR, findCustomDiv, splitString } from './utils.js';
 
 class App {
   async run() {
     while (true) {
-      let input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
-      if (!input || input === 'n') break;
-      let customString = null;
-      if (input[0] === '/') {
-        const cs = findCustomDiv(input);
-        if (!cs) {
-          // 예외처리
-        } else {
-          [customString, input] = cs;
+      try {
+        let input = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+        if (!input || input === 'n') break;
+        let customString = null;
+        if (input[0] === '/') {
+          const cs = findCustomDiv(input);
+          if (!cs) {
+            // 예외처리
+            throw new Error(`${ERROR_STR} 잘못된 커스텀 문자열 입력입니다.`);
+          } else {
+            [customString, input] = cs;
+          }
         }
+
+        const inputArr = splitString(input, [',', ':', customString]);
+
+        checkValid(inputArr);
+
+        const OUTPUT_STRING = '결과 : ';
+        MissionUtils.Console.print(OUTPUT_STRING + inputArr.reduce((acc, cur) => acc + +cur, 0));
+      } catch (e) {
+        MissionUtils.Console.print(e.message);
+        throw new Error(e);
       }
-
-      const inputArr = splitString(input, [',', ':', customString]);
-
-      const OUTPUT_STRING = '결과 : ';
-      MissionUtils.Console.print(OUTPUT_STRING + inputArr.reduce((acc, cur) => acc + +cur, 0));
     }
   }
 }
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+export const ERROR_STR = '[ERROR]';
+
 export const findCustomDiv = (input) => {
   if (input[0] === '/' && input[1] === '/') {
     const splitCustomInput = input.slice(2).split('\\n');
@@ -8,7 +10,30 @@ export const findCustomDiv = (input) => {
 };
 
 export const splitString = (input, div) => {
-  const separators = new RegExp(`[${div.join('')}]`);
+  let result = [input];
 
-  return input.split(separators);
+  for (const splitStr of div) {
+    const tmp = [];
+
+    for (const targetStr of result) {
+      tmp.push(...targetStr.split(splitStr));
+    }
+    result = tmp;
+  }
+
+  return result;
+};
+
+export const checkValid = (arr) => {
+  for (const num of arr) {
+    if (num === '' || num.trim() !== num) {
+      throw new Error('[ERROR] 빈 문자열 혹은 공백은 허용되지 않습니다.');
+    }
+    if (isNaN(num)) {
+      throw new Error('[ERROR] 숫자가 아닌 값이 포함되어 있습니다.');
+    }
+    if (Number(num) < 0) {
+      throw new Error('[ERROR] 음수는 허용되지 않습니다.');
+    }
+  }
 };
